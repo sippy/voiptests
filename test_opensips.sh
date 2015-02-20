6cf6ce6d56221c7c915bf909fe9cbbee4df41965
Maksym Sobolyev <sobomax@sippysoft.com>
2015-02-20 12:58:44 -0800

    Make it possible to test both b2bua and opensips as a MiM.

--- test_opensips.sh
+++ test_opensips.sh
@@ -10,19 +10,33 @@ ${CC} --version
 
 rtpproxy_cmds_gen() {
   sleep 30
-  echo "G nsess_created nsess_destroyed nsess_complete nsess_nortp nsess_owrtp nsess_nortcp nsess_owrtcp ncmds_rcvd ncmds_succd ncmds_errs ncmds_repld"
+  echo "Gv nsess_created nsess_destroyed nsess_complete nsess_nortp nsess_owrtp nsess_nortcp nsess_owrtcp ncmds_rcvd ncmds_succd ncmds_errs ncmds_repld"
 }
+
+start_mm() {
+  if [ "${MM_TYPE}" = "b2bua" ]
+  then
+    /usr/home/sobomax/projects/voiptests/dist/b2bua/sippy/b2bua_test.py --sip_address=127.0.0.1 \
+      --sip_port=5060 --foreground=on --acct_enable=off --auth_enable=off --static_route="127.0.0.1:5062" \
+      --b2bua_socket="/tmp/b2bua.sock" --rtp_proxy_clients="udp:127.0.0.1:22222" &
+    MM_PID=${!}
+    echo "${MM_PID}" > "${MM_PIDF}"
+  else
+    ./dist/opensips/opensips -f opensips.cfg -D -P "${MM_PIDF}" &
+    MM_PID=${!}
+  fi
+}  
 
 #"${BUILDDIR}/install_depends/opensips.sh"
 
 RTPP_PIDF="${BUILDDIR}/rtpproxy.pid"
-OPSPS_PIDF="${BUILDDIR}/opensips.pid"
+MM_PIDF="${BUILDDIR}/opensips.pid"
 ALICE_PIDF="${BUILDDIR}/alice.pid"
 BOB_PIDF="${BUILDDIR}/bob.pid"
 
 cd ${BUILDDIR}
 
-for pidf in "${RTPP_PIDF}" "${OPSPS_PIDF}" "${ALICE_PIDF}" "${BOB_PIDF}"
+for pidf in "${RTPP_PIDF}" "${MM_PIDF}" "${ALICE_PIDF}" "${BOB_PIDF}"
 do
   if [ ! -e "${pidf}" ]
   then
@@ -34,8 +48,8 @@ done
 
 rtpproxy_cmds_gen | ${RTPPROXY} -p "${RTPP_PIDF}" -d info -f -s stdio: -s udp:127.0.0.1:22222 -s cunix:/tmp/rtpproxy.csock -s unix:/tmp/rtpproxy.sock > rtpproxy.rout &
 RTPP_PID=${!}
-./dist/opensips/opensips -f opensips.cfg -D -P "${OPSPS_PIDF}" &
-OPENSIPS_PID=${!}
+start_mm
+echo "${MM_PID}" > "${MM_PIDF}"
 python alice.py 127.0.0.1 5061 &
 ALICE_PID=${!}
 echo "${ALICE_PID}" > "${ALICE_PIDF}"
@@ -49,10 +63,10 @@ wait ${ALICE_PID}
 ALICE_RC="${?}"
 wait ${BOB_PID}
 BOB_RC="${?}"
-kill -TERM ${OPENSIPS_PID}
-echo "OPENSIPS_PID: ${OPENSIPS_PID}"
-wait ${OPENSIPS_PID}
-OPENSIPS_RC="${?}"
+kill -TERM ${MM_PID}
+echo "MM_PID: ${MM_PID}"
+wait ${MM_PID}
+MM_RC="${?}"
 kill -HUP ${RTPP_PID}
 echo "RTPP_PID: ${RTPP_PID}"
 wait ${RTPP_PID}
@@ -60,10 +74,11 @@ RTPP_RC="${?}"
 
 rm -f "${ALICE_PIDF}" "${BOB_PIDF}"
 
-diff -u rtpproxy.rout rtpproxy.output
+diff -u rtpproxy.rout rtpproxy.${MM_TYPE}.output
 RTPP_CHECK_RC="${?}"
+
 report_rc "${ALICE_RC}" "Checking if Alice is happy"
 report_rc "${BOB_RC}" "Checking if Bob is happy"
 report_rc "${RTPP_RC}" "Checking RTPproxy exit code"
-report_rc "${OPENSIPS_RC}" "Checking OpenSIPS exit code"
+report_rc "${MM_RC}" "Checking ${MM_TYPE} exit code"
 report_rc "${RTPP_CHECK_RC}" "Checking RTPproxy stdout"
