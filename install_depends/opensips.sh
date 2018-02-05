403054116855cfa3fd456c882b6147c2494a2a4d
Maksym Sobolyev <sobomax@sippysoft.com>
2018-02-05 12:14:18 -0800

    Revert offending commits a45d4dcaf046bb273cfe5905ac035845a6867945 and dc258b09da9dcbc654f30365c6a0e8d4be015e6f in the opensips 2.2 and 2.3 respectively.

--- install_depends/opensips.sh
+++ install_depends/opensips.sh
@@ -45,11 +45,16 @@ then
       git -C opensips apply ${BUILDDIR}/install_depends/opensips/${p}
     done
   fi
-  #if [ "${MM_BRANCH}" = "2.3" ]
-  #then
-  #  git -C opensips revert -n 1eb4ec0f78f43f6ff546de49bc72e513876fb86b
+  if [ "${MM_BRANCH}" = "2.3" ]
+  then
+    git -C opensips revert -n a45d4dcaf046bb273cfe5905ac035845a6867945
   #  MM_KILL_MODULES="event_routing"
-  #fi
+  fi
+  if [ "${MM_BRANCH}" = "2.2" ]
+  then
+    git -C opensips revert -n dc258b09da9dcbc654f30365c6a0e8d4be015e6f
+  #  MM_KILL_MODULES="event_routing"
+  fi
 fi
 
 git clone git://github.com/sobomax/libelperiodic.git
