3a7ba79ba33782889a5681ea10d578e902a60bb1
Maksym Sobolyev <sobomax@sippysoft.com>
2020-08-31 16:06:53 -0700

    Verify failure with mismatching nonce flags.

--- test_cases/t1.py
+++ test_cases/t1.py
@@ -169,7 +169,7 @@ class b_test1(test):
               (self.my_name(), str(self.__class__), self.atype, why, in_body))
         if self.tccfg.uas_creds != None:
             if req.countHFs('authorization') == 0:
-                cbody = SipWWWAuthenticate(enabled_algos = (DGST_SHA256,))
+                cbody = SipWWWAuthenticate(enabled_algos = (DGST_MD5, DGST_SHA512,))
                 cbody.realm = req.getRURI().host
                 cbody.algorithm = 'SHA-256'
                 cbody.qop = ('auth',)
