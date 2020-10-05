beb266b26feae10fa07e8d123e939ecb57d7baa1
Maksym Sobolyev <sobomax@sippysoft.com>
2020-10-05 14:04:44 -0700

    Only enable MD5 here.

--- scenarios/UAC/bob_cfg.py
+++ scenarios/UAC/bob_cfg.py
@@ -1,7 +1,7 @@
 from lib.test_config import AUTH_CREDS as AUTH_CREDS_orig
 
 class AUTH_CREDS(AUTH_CREDS_orig):
-    #enalgs = ('SHA-512-256-sess', 'SHA-256-sess', 'MD5-sess', None)
+    enalgs = ('MD5',)
     realm = 'VoIPTests.NET'
 
     def __init__(self):
