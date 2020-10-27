28b031ce1e790a03e24a252c5578117b77fcc343
Maksym Sobolyev <sobomax@sippysoft.com>
2020-10-27 14:33:50 -0700

    Enable MD5-sess.

--- scenarios/UAC/bob_cfg.py
+++ scenarios/UAC/bob_cfg.py
@@ -1,7 +1,7 @@
 from lib.test_config import AUTH_CREDS as AUTH_CREDS_orig
 
 class AUTH_CREDS(AUTH_CREDS_orig):
-    enalgs = ('SHA-512-256', 'SHA-256', 'MD5',)
+    enalgs = ('SHA-512-256', 'SHA-256', 'MD5', 'MD5-sess')
     realm = 'VoIPTests.NET'
 
     def __init__(self):
