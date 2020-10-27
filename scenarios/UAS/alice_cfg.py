92b49051b9d1afb39f95d08e10559fe4389e051a
Maksym Sobolyev <sobomax@sippysoft.com>
2020-10-27 14:01:32 -0700

    Enable MD5-sess.

--- scenarios/UAS/alice_cfg.py
+++ scenarios/UAS/alice_cfg.py
@@ -1,7 +1,7 @@
 from lib.test_config import AUTH_CREDS as AUTH_CREDS_orig
 
 class AUTH_CREDS(AUTH_CREDS_orig):
-    enalgs = ('SHA-256', 'MD5', None)
+    enalgs = ('SHA-256', 'MD5-sess', None)
 
     def __init__(self):
         AUTH_CREDS_orig.__init__(self, 'mightyuser', 's3cr3tpAssw0Rd')
