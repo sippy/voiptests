2fab6dc582e1ebb1987b6e7fd9c27edddb198cd5
Maksym Sobolyev <sobomax@sippysoft.com>
2020-11-02 09:43:10 -0800

    Cycle over all different individual algorithms.

--- scenarios/UAS/alice_cfg.py
+++ scenarios/UAS/alice_cfg.py
@@ -1,7 +1,10 @@
 from lib.test_config import AUTH_CREDS as AUTH_CREDS_orig
 
-class AUTH_CREDS(AUTH_CREDS_orig):
-    enalgs = ('SHA-256', 'SHA-256-sess', 'MD5-sess', None)
+allargs = [(None,), ('MD5',), ('MD5-sess',), ('SHA-256',),  ('SHA-256-sess',),
+  ('SHA-256', 'SHA-256-sess', 'MD5-sess', None)]
 
+class AUTH_CREDS(AUTH_CREDS_orig):
     def __init__(self):
+        self.enalgs = allargs.pop()
+        allargs.insert(0, self.enalgs)
         AUTH_CREDS_orig.__init__(self, 'mightyuser', 's3cr3tpAssw0Rd')
