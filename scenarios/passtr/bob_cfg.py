c3937702d8d9fa4ef7661a555876ad69654f88fd
Maksym Sobolyev <sobomax@sippysoft.com>
2020-11-25 15:24:20 -0800

    Enable all auth algorithms that might be emitted by the alice.

--- scenarios/passtr/bob_cfg.py
+++ scenarios/passtr/bob_cfg.py
@@ -1,1 +1,8 @@
-../UAC/bob_cfg.py
+from lib.test_config import AUTH_CREDS as AUTH_CREDS_orig
+
+class AUTH_CREDS(AUTH_CREDS_orig):
+    enalgs = ('SHA-256-sess', 'SHA-256', 'MD5-sess', 'MD5', None)
+    realm = 'VoIPTests.NET'
+
+    def __init__(self):
+        AUTH_CREDS_orig.__init__(self, 'mightyuser', 's3cr3tpAssw0Rd')
