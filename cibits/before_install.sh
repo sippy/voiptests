875b22de788155c953e28c2b4e5cea3bf8451aa6
Maksym Sobolyev <sobomax@sippysoft.com>
2020-11-25 14:27:03 -0800

    Merge from RFC8760 branch:
     o install requirements.txt if exists;
    
     o install gdb;
    
     o enable core dumps.

--- cibits/before_install.sh
+++ cibits/before_install.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+BASEDIR="${BASEDIR:-$(dirname -- "${0}")/..}"
+BASEDIR="$(readlink -f -- ${BASEDIR})"
+
+. ${BASEDIR}/functions
+
 echo "+++ hosts orig +++"
 cat /etc/hosts
 echo "--- hosts orig ---"
@@ -10,6 +15,12 @@ sudo sh -c 'echo >> /etc/hosts; grep "^127.0.0.1" /etc/hosts | sed "s|^127.0.0.1
 echo "+++ hosts patched +++"
 cat /etc/hosts
 echo "--- hosts patched ---"
+ulimit -c unlimited -S
+sudo sh -c "echo '/tmp/core.%p.%E' > /proc/sys/kernel/core_pattern"
 sudo -H DEBIAN_FRONTEND=noninteractive apt-get update
-sudo -H DEBIAN_FRONTEND=noninteractive apt-get install gpp
+sudo -H DEBIAN_FRONTEND=noninteractive apt-get install -y gpp gdb
+if [ -e requirements.txt ]
+then
+  sudo ${PYTHON_CMD} -m pip install -r requirements.txt
+fi
 sh -x ./install_depends/opensips.sh
