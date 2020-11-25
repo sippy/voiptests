7be962076c257931abfecbb72de6dadd1721229f
Maksym Sobolyev <sobomax@sippysoft.com>
2020-11-25 15:30:07 -0800

    Shuffle order in which algorithms are to be accepted to test all of them.

--- scenarios/passtr/bob_cfg.py
+++ scenarios/passtr/bob_cfg.py
@@ -1,8 +1,14 @@
 from lib.test_config import AUTH_CREDS as AUTH_CREDS_orig
+from random import shuffle
+
+allalgs = ('SHA-256-sess', 'SHA-256', 'MD5-sess', 'MD5', None)
 
 class AUTH_CREDS(AUTH_CREDS_orig):
-    enalgs = ('SHA-256-sess', 'SHA-256', 'MD5-sess', 'MD5', None)
+    enalgs = None
     realm = 'VoIPTests.NET'
 
     def __init__(self):
+        tlist = list(enalgs)
+        shuffle(tlist)
+        self.enalgs = tuple(tlist)
         AUTH_CREDS_orig.__init__(self, 'mightyuser', 's3cr3tpAssw0Rd')
