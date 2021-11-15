c2236c94cb165ec3c67b36c79bba3b1534aa6ea7
Maksym Sobolyev <sobomax@sippysoft.com>
2021-11-15 08:10:05 +0000

    Make UAC go over all supported -sess variants by introducing variability into what Bob offers.

--- scenarios/UAC/bob_cfg.py
+++ scenarios/UAC/bob_cfg.py
@@ -1,8 +1,15 @@
 from lib.test_config import AUTH_CREDS as AUTH_CREDS_orig
 
+allargs = (('SHA-512-256', 'SHA-256', 'MD5', 'MD5-sess'), \
+  ('SHA-512-256', 'SHA-256', 'SHA-256-sess', 'MD5'), \
+  ('SHA-512-256', 'SHA-512-256-sess', 'SHA-256', 'MD5')) \
+
 class AUTH_CREDS(AUTH_CREDS_orig):
-    enalgs = ('SHA-512-256', 'SHA-256', 'MD5', 'MD5-sess')
+    enalgs = None
     realm = 'VoIPTests.NET'
 
     def __init__(self):
+        tlist = list(allalgs)
+        shuffle(tlist)
+        self.enalgs = tlist[0]
         AUTH_CREDS_orig.__init__(self, 'mightyuser', 's3cr3tpAssw0Rd')
