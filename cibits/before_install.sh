5114f103c7d7ee4c4524d030e624cd09b4bfa99d
Maksym Sobolyev <sobomax@sippysoft.com>
2024-07-31 09:57:49 -0700

    Don't use sudo if we are root already.

--- cibits/before_install.sh
+++ cibits/before_install.sh
@@ -10,18 +10,18 @@ BASEDIR="$(readlink -f -- ${BASEDIR})"
 echo "+++ hosts orig +++"
 cat /etc/hosts
 echo "--- hosts orig ---"
-sudo sh -c 'echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6'
-sudo sh -c 'echo >> /etc/hosts; grep "^127.0.0.1" /etc/hosts | sed "s|^127.0.0.1|::1|g" >> /etc/hosts'
+${SUDO} sh -c 'echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6'
+${SUDO} sh -c 'echo >> /etc/hosts; grep "^127.0.0.1" /etc/hosts | sed "s|^127.0.0.1|::1|g" >> /etc/hosts'
 echo "+++ hosts patched +++"
 cat /etc/hosts
 echo "--- hosts patched ---"
 ulimit -c unlimited
-sudo sh -c "echo '/tmp/core.%p.%E' > /proc/sys/kernel/core_pattern"
+${SUDO} sh -c "echo '/tmp/core.%p.%E' > /proc/sys/kernel/core_pattern"
 #${BASEDIR}/cibits/ppa_clean.sh
-sudo -H DEBIAN_FRONTEND=noninteractive apt-get update
-sudo -H DEBIAN_FRONTEND=noninteractive apt-get install -y gpp gdb tcpdump
+${SUDO} env DEBIAN_FRONTEND=noninteractive apt-get update
+${SUDO} env DEBIAN_FRONTEND=noninteractive apt-get install -y gpp gdb tcpdump
 if [ -e requirements.txt ]
 then
-  sudo ${PYTHON_CMD} -m pip install -r requirements.txt
+  ${SUDO} ${PYTHON_CMD} -m pip install -r requirements.txt
 fi
 sh -x ./install_depends/opensips.sh
