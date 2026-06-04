e506dd4c53e36b7e481098b291d5b35021e04d47
Maksym Sobolyev <sobomax@sippysoft.com>
2026-06-04 12:03:26 -0700

    Add cache-from / cache-to to speed-up SSUT image builds.

---
+++ cibits/install_ssut_bdeps.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -eux
+
+apt-get update
+apt-get install -y --no-install-recommends \
+  python3 python3-pip \
+  gpp gdb tcpdump gcc libc6-dev git make ca-certificates \
+  libssl-dev flex bison
+
+if [ "${MM_TYPE}" = "go-b2bua" ]
+then
+  apt-get install -y --no-install-recommends golang-go
+fi
+
+if [ "${MM_TYPE}" = "kamailio" ]
+then
+  apt-get install -y --no-install-recommends libcurl4-gnutls-dev
+fi
